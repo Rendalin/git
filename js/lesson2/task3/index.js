fb551adc194bd01dbc61bfa4dd9fd0086e0412ce
Mona Lisa <Mona Lisa>
2020-06-08 13:13:08 +0300

    task

--- js/lesson2/task3/index.js
+++ js/lesson2/task3/index.js
@@ -2,12 +2,12 @@ const goodNumber = 17;
 let anotherNumber = goodNumber;
 anotherNumber = 77;
 
-Console.log(goodNumber);
-Console.log(anotherNumber);
+console.log(goodNumber);
+console.log(anotherNumber);
 
 const customer = { name: 'John', age: 17 };
 const anotherCustomer = customer;
 anotherCustomer.age = 20;
 
-Console.log(customer.age);
-Console.log(anotherCustomer.age);
+console.log(customer.age);
+console.log(anotherCustomer.age);
