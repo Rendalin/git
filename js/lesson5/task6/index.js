b321d04d4b23f380efd1dcb7819f1e5a1840d751
Mona Lisa <Mona Lisa>
2020-06-12 12:42:38 +0300

    task

--- js/lesson5/task6/index.js
+++ js/lesson5/task6/index.js
@@ -1,10 +1,14 @@
-function getPrimes(n) {
-    for (const i = 2; i <= n; i++) {
-        for (const j = 2; j < i; j++) {
-            if (i % j == 0) continue getPrimes;
+function getPrimes(a) {
+    for (let i = 2; i <= a; i += 1) {
+        let counter = 0;
+        for (let j = 2; j < i; j += 1) {
+            if (i % j === 0) {
+                counter += 1;
+            }
+        }
+        if (counter === 0) {
+            console.log(i);
         }
     }
-    alert(i)
 }
-
-const n = 10;
+const a = 10;
