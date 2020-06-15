cc02b8acb47121f7134bfe1f8bf6e0a4fe70676f
Mona Lisa <Mona Lisa>
2020-06-15 12:07:50 +0300

    task

--- js/lesson6/task2/index.js
+++ js/lesson6/task2/index.js
@@ -1,12 +1,12 @@
 function getSum(arr) {
     sumNambers = 0;
-    for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i]);
-        sumNambers += arr[i];
-        return sumNambers;
-    }
     if (!Array.isArray(arr)) {
         return null;
     }
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+        sumNambers += arr[i];
+    }
 
+    return sumNambers;
 }
