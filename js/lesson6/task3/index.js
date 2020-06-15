816c2f9d3bcc28cd6715b244ba3a6d84014c8ede
Mona Lisa <Mona Lisa>
2020-06-15 13:49:26 +0300

    task

--- js/lesson6/task3/index.js
+++ js/lesson6/task3/index.js
@@ -1,9 +1,11 @@
 function getSpecialNumbers(m, n) {
     let specialNumbers = [];
     for (let i = m; i < n; i++) {
-        if (i % 3 === 0); {
+        if (i % 3 === 0) {
             specialNumbers.push(i);
         }
     }
     return specialNumbers;
 }
+
+console.log(getSpecialNumbers(30, 35));
