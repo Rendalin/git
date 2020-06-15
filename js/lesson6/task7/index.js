af36a9f2385a0c8daed8452577ca37fd0be3fade
Mona Lisa <Mona Lisa>
2020-06-15 20:07:00 +0300

    task

---
+++ js/lesson6/task7/index.js
@@ -0,0 +1,10 @@
+function squareArray(arr) {
+    let arrInSquare = [];
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        arrInSquare.push(Math.pow(arr[i], 2));
+    }
+    return arrInSquare;
+}
