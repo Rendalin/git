79410f30a6478abdef6b46b7ad8cd95fa35fc036
Mona Lisa <Mona Lisa>
2020-06-15 20:18:55 +0300

    task

--- js/lesson6/task8/index.js
+++ js/lesson6/task8/index.js
@@ -3,7 +3,7 @@ function checkSum(arr) {
         return null;
     }
     let sum = 0;
-    for (let i of arr) {
+    for (let i of arr) { /// for(let i = 0; i<=arr.length; i++)
         sum += i;
     }
     return sum > 100 ? true : false;
