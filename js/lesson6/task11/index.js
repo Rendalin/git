f43e859ee09191286832fdff786dde95e77518a3
Mona Lisa <Mona Lisa>
2020-06-16 23:40:43 +0300

    task

--- js/lesson6/task11/index.js
+++ js/lesson6/task11/index.js
@@ -4,10 +4,10 @@ function cloneArr(arr) {
     }
     if (Array.isArray(arr)) {
         let newarr = [];
-    }
-    for (let i = 0; i <= arr.length - 1; i++) {
-        newarr.push(arr[i]);
+        for (let i = 0; i <= arr.length - 1; i++) {
+            newarr.push(arr[i]);
 
+        }
+        return newarr;
     }
-    return newarr;
 }
