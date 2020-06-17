e91b3e57d1e2925db98120a82d6879e57a7a5e0a
Mona Lisa <Mona Lisa>
2020-06-17 04:15:16 +0300

    task

--- js/lesson6/task18/index.js
+++ js/lesson6/task18/index.js
@@ -1,11 +1,11 @@
-function removeDuplicates(array) {
+function uniqueCount(array) {
     if (!Array.isArray(array)) {
         return null;
     }
-    let arrDuplicates = [];
-    for (let i = 0; i < array.length; i++) {
+    const arrDuplicates = [];
+    for (let i = 0; i < array.length; i += 1) {
         arrDuplicates.push(array[i]);
-        for (let j = i + 1; j < array.length; j++) {
+        for (let j = i + 1; j < array.length; j += 1) {
             if (array[j] === array[i]) {
                 arrDuplicates.pop(array[j]);
             }
