bba73b2924103f27c310079b0ce996dcdf0d2b53
Mona Lisa <Mona Lisa>
2020-06-20 12:11:33 +0300

    task

---
+++ js/lesson7/task9/index.js
@@ -0,0 +1,4 @@
+const withdraw = (clients, balances, client, amount) =>
+    clients.findIndex(elem => (elem === client));
+return balances[clientId] = amount > balances[clientId] ?
+    -1 : balances[clientId] - amount;
