13123f233ed64f113edcb996bcc6ac8403c6b930
Mona Lisa <Mona Lisa>
2020-06-21 14:08:54 +0300

    task

--- js/lesson8/task7/index.js
+++ js/lesson8/task7/index.js
@@ -2,7 +2,9 @@ const addPropertyV1 = (obj, key, value) => obj[key] = value;
 const addPropertyV2 = (obj, key, value) => Object.assign(obj, {
     [key]: value
 });
-const addPropertyV3 = (obj, key, value) => copy = Object.assign({}, obj, {
-    [key]: value
-});
-const addPropertyV4 = (obj, key, value) => copyObj = {...obj, [key]: value };
+const addPropertyV3 = (obj, key, value) => {
+    const copy = Object.assign({}, obj, {
+        [key]: value
+    })
+};
+const addPropertyV4 = (obj, key, value) => { copyObj = {...obj, [key]: value } };
