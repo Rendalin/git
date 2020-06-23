8483a9a8de7285ee1e38b6054a7fae130795ad4e
Mona Lisa <Mona Lisa>
2020-06-23 11:21:57 +0300

    task

--- js/lesson8/task9/index.js
+++ js/lesson8/task9/index.js
@@ -1,9 +1,10 @@
-const compareObjects = (obj1, obj2) => {
-    if (obj1 == obj2) {
-        return false;
-    } else {
-        if (obj1 !== obj2) {
-            return true;
-        }
+function compareObjects(obj1, obj2) {
+    const arr1 = Object.values(obj1).sort().toString();
+    const arr2 = Object.values(obj2).sort().toString();
+    const arr3 = Object.keys(obj1).sort().toString();
+    const arr4 = Object.keys(obj2).sort().toString();
+    if (arr1 === arr2 && arr3 === arr4) {
+        return true;
     }
-};
+    return false;
+}
