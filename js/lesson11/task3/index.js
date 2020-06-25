a5d6badd66548e81387ba74bd9e01a8be24d8433
Mona Lisa <Mona Lisa>
2020-06-25 23:53:28 +0300

    task

---
+++ js/lesson11/task3/index.js
@@ -0,0 +1,22 @@
+const splitString = (string, len = 10) => {
+    if (typeof string !== 'string')
+        return null;
+
+    const strArr = [];
+    let start = 0;
+
+    while (true) {
+        let chunk = string.substr(start, len);
+
+        if (chunk.length === 0)
+            break;
+
+        if (chunk.length < len)
+            chunk += '.'.repeat(len - chunk.length)
+
+        strArr.push(chunk);
+        start += len;
+    }
+
+    return strArr;
+};
