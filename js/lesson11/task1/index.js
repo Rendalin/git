b158f9d13bd01fc854ef2f611891205ef818e8de
Mona Lisa <Mona Lisa>
2020-06-25 23:42:18 +0300

    task

--- js/lesson11/task1/index.js
+++ js/lesson11/task1/index.js
@@ -1,15 +1,14 @@
 const splitText = (text, num = 10) => {
-    if (text != '') {
+    if (typeof text != 'string')
         return null;
-    }
 
     const arr = [];
     let startPositon = 0;
     while (true) {
         let chanc = text.substr(startPositon, num);
-        if (chanc === 0) {
+        if (chanc.length === 0)
             break;
-        }
+
         arr.push(chanc[0].toUpperCase() + chanc.slise[1]);
         startPositon += num;
     }
