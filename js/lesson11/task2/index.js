1e341c8c3db98bf1ad95c59898bc2214ac92de26
Mona Lisa <Mona Lisa>
2020-06-25 23:47:23 +0300

    task

---
+++ js/lesson11/task2/index.js
@@ -0,0 +1,12 @@
+const sortContacts = (contactsArr, Boolean = true) => {
+
+    if (!Array.isArray(contactsArr))
+        return null;
+
+    const result = contactsArr.sort((a, b) => {
+        if (Boolean === false)
+            return b.name.localeCompare(a.name);
+        return a.name.localeCompare(b.name);
+    });
+    return result;
+};
