6df8d8fa3f15c544e12deb09847a47a55dc86dc6
Mona Lisa <Mona Lisa>
2020-07-02 15:19:11 +0300

    task

---
+++ js/lesson13/task2/calculator.test.js
@@ -0,0 +1,19 @@
+import getSum, { getSquaredArray, getOddNumbers } from './calculator'
+
+it('Возводим масив в квадрат', () => {
+    const result = getSquaredArray([2]);
+
+    expect(result).toEqual([4]);
+})
+
+it('Выводим в новый масив все не четные числа', () => {
+    const result = getOddNumbers([1, 2, 3, 4, 5]);
+
+    expect(result).toEqual([1, 3, 5]);
+})
+
+it('Выводим суму элементов массива', () => {
+    const result = getSum(8, 4);
+
+    expect(result).toEqual(12);
+})
