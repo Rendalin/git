d00430480d676dbe62a704db2f854260bd416114
Mona Lisa <Mona Lisa>
2020-07-02 21:48:58 +0300

    task

---
+++ js/lesson13/task4/calculator.test.js
@@ -0,0 +1,27 @@
+import { calc } from './calculator'
+
+it('Работа +', () => {
+    const result = calc('2 + 2');
+
+    expect(result).toEqual('2 + 2 = 4');
+})
+it('Работа -', () => {
+    const result = calc('2 - 2');
+
+    expect(result).toEqual('2 - 2 = 0');
+})
+it('Работа *', () => {
+    const result = calc('2 * 2');
+
+    expect(result).toEqual('2 * 2 = 4');
+})
+it('Работа /', () => {
+    const result = calc('2/2');
+
+    expect(result).toEqual('2 / 2 = 1');
+})
+it('Если передана не строка', () => {
+    const result = calc(2 + 2);
+
+    expect(result).toEqual(null);
+})
